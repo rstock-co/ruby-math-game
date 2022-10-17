ac5829e436969f68ae58bb5befb0693fc8bc0940
Richard Stock <rstock.co@gmail.com>
2022-10-17 16:45:25 -0600

    Minor code cleanup

--- game.rb
+++ game.rb
@@ -9,7 +9,7 @@ class Game
     @player_2 = Player.new(name_2)
   end
 
-  def current_player()
+  def player_order()
     @turn_number += 1
     if @turn_number % 2 === 0 
       return [@player_2,@player_1]
@@ -17,22 +17,21 @@ class Game
     return [@player_1,@player_2]
   end
 
+  def new_turn()
+    turn(player_order())
+  end
+
   def turn(players)
     turn = Turn.new(players[0])
     p turn.ask_question()
+
     if players[0].lives === 0
       return p "Sorry #{players[0].name}, you've lost. #{players[1].name} won, with a score of #{players[1].lives}/3!"
     end
+    
     new_turn()
   end
 
-  def evaluate_game()
-    
-  end
-
-  def new_turn()
-    turn(current_player())
-  end
 end
 
   
